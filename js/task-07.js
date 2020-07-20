190f82974e667c579768466203ad3e425f236197
Pavel Rybalko <freedom277f@gmail.com>
2020-07-20 20:00:56 +0300

    prettier fixes

--- js/task-07.js
+++ js/task-07.js
@@ -1,26 +1,27 @@
-const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = function (login) {
-  return (login.length >= 4 && login.length <= 16);
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function (allLogins, login) {
-return (allLogins.includes(login));
+  return allLogins.includes(login);
 };
 
 const addLogin = function (allLogins, login) {
-
-	if(!isLoginValid(login)){
-		return "Ошибка! Логин должен быть от 4 до 16 символов";
+  if (!isLoginValid(login)) {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
   }
-	if(!isLoginUnique(allLogins, login)){
-  		return 'Такой логин уже используется!';
-  	}
-  		allLogins.push(login);
-  		return "Логин Успешно добавлен";
+
+  if (!isLoginUnique(allLogins, login)) {
+    return 'Такой логин уже используется!';
+  }
+
+  allLogins.push(login);
+  return 'Логин Успешно добавлен';
 };
 
-console.log(addLogin(logins, "Ajax")); 
-console.log(addLogin(logins, "robotGoogles")); 
-console.log(addLogin(logins, "Zod"));
-console.log(addLogin(logins, "jqueryisextremelyfast"));
+console.log(addLogin(logins, 'Ajax'));
+console.log(addLogin(logins, 'robotGoogles'));
+console.log(addLogin(logins, 'Zod'));
+console.log(addLogin(logins, 'jqueryisextremelyfast'));
