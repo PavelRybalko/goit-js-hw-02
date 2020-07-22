e2aaa9742a133dd948ed11ff850919b65b93e03e
Pavel Rybalko <freedom277f@gmail.com>
2020-07-22 20:34:15 +0300

    last mentor fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,21 +3,20 @@ const numbers = [];
 let total = 0;
 
 do {
-  input = prompt("Введите число");
+  input = prompt('Введите число');
 
-  if (input !== null && !Number.isNaN(+input)) {
+  if (input !== null) {
+    if (Number.isNaN(+input)) {
+      alert('Было введено не число, попробуйте ещё раз');
+      continue;
+    }
     numbers.push(input);
   }
-
-  if (Number.isNaN(+input)) {
-    alert("Было введено не число, попробуйте ещё раз");
-  }
-
 } while (input !== null);
 
-if (numbers.lenght !== 0) {
+if (numbers.length) {
   for (const number of numbers) {
     total += +number;
   }
-  console.log("Общая сумма чисел равна", total);
+  console.log('Общая сумма чисел равна', total);
 }
